436b6e8a4eb80b6747c781cb6922cd4c703fe2c2
Hyeon Seong LEE <dynamic1117@gmail.com>
2021-11-07 21:32:04 +0900

    fix deploy mistake

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
